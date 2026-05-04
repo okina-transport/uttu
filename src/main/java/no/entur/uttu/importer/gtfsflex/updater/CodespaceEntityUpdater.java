9efbf78b6ad96e65fac436e8bfe526df969afd49
afreche <alexandre.freche@lumiplan.com>
2026-05-04 17:34:28 +0200

    fix(): do not update codespace, network and provider because they are retrieved from CHOUETTE

--- src/main/java/no/entur/uttu/importer/gtfsflex/updater/CodespaceEntityUpdater.java
+++ src/main/java/no/entur/uttu/importer/gtfsflex/updater/CodespaceEntityUpdater.java
@@ -1,27 +1,10 @@
 package no.entur.uttu.importer.gtfsflex.updater;
 
 import no.entur.uttu.model.Codespace;
-import org.apache.commons.lang3.Strings;
 import org.springframework.stereotype.Component;
 
 @Component
+// there is nothing to update at the moment, but this class is required for the interface
 public class CodespaceEntityUpdater implements EntityUpdater<Codespace> {
-
-    @Override
-    public boolean update(Codespace newEntity, Codespace dbEntity) {
-        boolean updated = false;
-        
-        if (!Strings.CS.equals(newEntity.getXmlns(), dbEntity.getXmlns())) {
-            dbEntity.setXmlns(newEntity.getXmlns());
-            updated = true;
-        }
-
-        if (!Strings.CS.equals(newEntity.getXmlnsUrl(), dbEntity.getXmlnsUrl())) {
-            dbEntity.setXmlnsUrl(newEntity.getXmlnsUrl());
-            updated = true;
-        }
-
-        return updated;
-    }
 
 }
--- src/main/java/no/entur/uttu/repository/NetworkRepository.java
+++ src/main/java/no/entur/uttu/repository/NetworkRepository.java
@@ -4,6 +4,11 @@ import no.entur.uttu.model.Network;
 import no.entur.uttu.repository.generic.ProviderEntityRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface NetworkRepository extends ProviderEntityRepository<Network> {
+
+    Optional<Network> findByName(String name);
+
 }
